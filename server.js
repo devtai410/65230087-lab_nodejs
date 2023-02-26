ae612ac440142a661d06afc37ee46bedadd1f6ba
devtai410 <waiyawootlove@gmail.com>
2023-02-26 14:11:21 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 9000;
+const cors = require("cors");
+
+const fullname = "65230087 วัยวุฒิ ชุมเมืองปัก";
+//  data
+const products = [
+  {
+    id: "1",
+    name: "Apple Watch series 8",
+    category: "Apple",
+    price: 15000,
+    cover:
+      "https://www.flashfly.net/wp/wp-content/uploads/2022/05/FTX1B5hWIAA6GRr-2-800x600.jpeg",
+  },
+  {
+    id: "2",
+    name: "Iphone 14 PRO",
+    category: "Apple",
+    price: 35000,
+    cover:
+      "https://promotions.co.th/wp-content/uploads/2022/09/iphone14-pro-max.jpg",
+  },
+  {
+    id: "3",
+    name: "จอมอนิเตอร์ 27นิ้ว จอคอม 75HZ หน้าจอโค้ง จอเกมมิ่ง LED Gaming monitor จอมอนิเตอร์เกมมิ่ง VGA HDMI จอมอนิเตอ สปอตสินค้า",
+    category: "LG",
+    price: 5600,
+    cover:
+      "https://www.techmoblog.com/uploads/content_images/201610/img_1476165152_81877887513b.jpg",
+  },
+  {
+    id: "4",
+    name: "HEADSET (หูฟัง) ONIKUMA K9 - 3.5 MM (PINK)",
+    category: "ONIKUMA ",
+    price: 990,
+    cover:
+      "https://www.jib.co.th/img_master/product/original/20210803092821_41661_66_1.jpg",
+  },
+  {
+    id: "5",
+    name: "Apple iPad Gen 9 | iStudio by copperwired",
+    category: "Apple",
+    price: 12900,
+    cover:
+      "https://store.ais.co.th/media/catalog/product/i/p/ipad_10.2_inch_wi-fi_space_gray_pdp_image_position-1b__th_2.jpg",
+  },
+  {
+    id: "6",
+    name: "น้องมาย",
+    category: "ไซด์ไลน์",
+    price: 1500,
+    cover:
+      "https://scontent.fbkk15-1.fna.fbcdn.net/v/t39.30808-6/298916475_525632722700791_6657034722643333909_n.png?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=1YG6mmLLXd4AX-wGUAA&_nc_ht=scontent.fbkk15-1.fna&oh=00_AfC570kh5TGpFBR5r_xw-BDz-UGO6BhDjUcg76fZayLDkQ&oe=63FFD008",
+  },
+];
+
+app.use(express.json(), cors());
+// app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ message: fullname });
+});
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const result = products.find((product) => product.id === id);
+  res.json(result);
+});
+
+app.listen(port, () => {
+  console.log("Application is running on port 9000");
+});
